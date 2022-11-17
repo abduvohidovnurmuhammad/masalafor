c5ad8a5d3596bec6262e3a59e0119fd1a193cf85
abduvohidovnurmuhammad <abduvohidovn7@gmail.com>
2022-11-17 15:48:18 +0500

    masala yechildi forga doir

---
+++ main.js
@@ -0,0 +1,29 @@
+//  ------------tub son topish ---------------------
+//  let son = prompt("... shu son ichida nechta toq son borligini bilish")
+// for( let i = 0; i<son; i++ ){
+//   if(i%2 ||i%i){
+//     console.log(i);
+//   }
+// }
+// --------------1-masala---------------------
+
+    // let k = prompt("son kiriting")
+    // for( let n=0;  n <= k; n++ ){
+    //   console.log(n);
+    // }
+    // --------------2-masala----------------
+      let kontent = prompt("nech kilo olasiz kiriting narxini aytamiz ")
+      let narx = 7500
+      for(let i = 0 ; i>kontent;i++){
+        console.log(i)
+      }
+      console.log(kontent*7500);
+      // -------------3-masala----------------
+
+    // let number = prompt("son kiriting")
+    // for( let i=0;  i <=number; i++ ){
+    //   console.log(number*i);
+    // }
+
+         
+      
